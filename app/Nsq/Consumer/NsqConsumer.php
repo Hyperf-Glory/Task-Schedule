34ec240c142a023e8e4dde6236aed3b45b3eb3a6
he ping <847050412@qq.com>
2021-02-18 18:33:40 +0800

    Add Example

--- app/Nsq/Consumer/NsqConsumer.php
+++ app/Nsq/Consumer/NsqConsumer.php
@@ -11,6 +11,8 @@ use Hyperf\Nsq\Message;
 use Hyperf\Nsq\Result;
 use App\Component\Serializer\JsonSerializer;
 use App\Component\Serializer\ObjectSerializer;
+use InvalidArgumentException;
+use Hyperf\Utils\Pipeline;
 
 /**
  * @Consumer(topic="task-schedule-queue", channel="task-schedule", name ="NsqConsumer", nums=1)
@@ -27,23 +29,40 @@ class NsqConsumer extends AbstractConsumer
      */
     protected $objectSerializer;
 
+    /**
+     * @var Pipeline
+     */
+    protected $pipeline;
+
     public function consume(Message $message) : ?string
     {
 
         $this->jsonSerializer   = $this->container->get(JsonSerializer::class);
         $this->objectSerializer = $this->container->get(ObjectSerializer::class);
+        $this->pipeline         = $this->container->get(Pipeline::class);
         $body                   = $this->jsonSerializer->denormalize($message->getBody());
         if ($job = $this->objectSerializer->denormalize($body['serializedMessage'])) {
-
+            $this->handle($job);
         }
 
         return Result::ACK;
     }
 
+    /**
+     * @param JobInterface|\Closure $handler
+     */
     protected function handle($handler) : void
     {
         try {
-
+            if (empty($handler)) {
+                throw new InvalidArgumentException('Job is empty.');
+            }
+            is_callable($handler) ? $handler() : $this->pipeline->send($handler)
+                                                                ->through($handler->middleware())
+                                                                ->then(function (JobInterface $job)
+                                                                {
+                                                                    $job->handle();
+                                                                });
         } catch (\Throwable $throwable) {
 
         }
